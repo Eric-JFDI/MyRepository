c9c0a5ea745ec9def0468ea21c64883598cfb9b9
HaKuNa <734216267@qq.com>
2019-09-14 00:01:29 +0800

    My First commit for nextGit

---
+++ src/com/zhanlang/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.zhanlang;
+
+/**
+ * Created by HaKuNa on 2019/9/13.
+ */
+public class HelloWorld {
+
+    private String name;
+
+    private String age;
+
+    private String sex;
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello,World!");
+    }
+}
